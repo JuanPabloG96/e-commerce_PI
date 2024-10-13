ec73efec77af042a7f6e9a11061e8f6f830121ce
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2024-10-13 16:51:30 -0600

    update: server db connection

--- server/create_db.sql
+++ server/create_db.sql
@@ -17,8 +17,6 @@ CREATE TABLE IF NOT EXISTS products (
     name VARCHAR(100) NOT NULL,
     description TEXT,
     price DECIMAL(10, 2) NOT NULL,
-    stock INT NOT NULL,
-    category VARCHAR(50),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- server/db_connection.php
+++ server/db_connection.php
@@ -1,14 +1,18 @@
 <?php
-$host = '192.168.1.100';
+// Asigna las variables de entorno a variables PHP
+$host = 'localhost';
 $db   = 'hashers_ecommerce';
 $user = 'john';
 $pass = 'admin';
 
+// Conecta a la base de datos
 $conn = new mysqli($host, $user, $pass, $db);
 
+// Verifica si hay un error de conexión
 if ($conn->connect_error) {
     die('Error de conexión (' . $conn->connect_errno . ') '
             . $conn->connect_error);
 }
 
+// Establece el conjunto de caracteres a utf8mb4
 $conn->set_charset("utf8mb4");
